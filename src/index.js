9315b70245da98ddc23f4871f24d3e32d542d146
Gerard Muñoz <gmunoz1979@gmail.com>
2017-05-23 11:43:51 -0400

    Configuración moment, carga de iconos y visibilidad de componentes en index

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import moment from 'moment';
+moment.locale('es');
+
+import components from './components';
+const {Calendar, Cell} = components;
+import 'material-icons';
+
+export {Calendar, Cell, components};
+
+export default {
+  Calendar,
+  Cell,
+  components
+};
