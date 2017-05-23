2a1ffc42ac854ec6a390ecc90091b6d765217a8c
Gerard Muñoz <gmunoz1979@gmail.com>
2017-05-23 11:44:17 -0400

    Test se anaden fechas diferentes en los calendarios

--- test/index.jsx
+++ test/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Calendar} from '../src/components';
+import {Calendar} from '../src';
+import moment from 'moment';
 
 // Styles
 import '../src/sass/default.scss';
@@ -14,7 +15,11 @@ container1.classList.add('container1');
 container1.style.width = '300px';
 container1.style.height= '100%';
 document.body.appendChild(container1);
-render(<Calendar type='daily' />, container1);
+
+render(<Calendar
+  type='daily'
+  date={moment().add(1, 'days')}
+/>, container1);
 
 const container2 = document.createElement('div');
 container2.classList.add('container1');
@@ -22,4 +27,7 @@ container2.style.width = '300px';
 container2.style.height= '100%';
 document.body.appendChild(container2);
 
-render(<Calendar type='monthly' />, container2);
+render(<Calendar
+  type='monthly'
+  date={moment().add(1, 'months')}
+/>, container2);
